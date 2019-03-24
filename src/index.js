3a7761eead55c011ba47273d02f0c741dbfb6b44
mandrik.zhenya <mandrik.zhenya1@gmail.com>
2019-03-24 20:52:26 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,58 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let i = 2, _base = base, n = 1, count = 0;
+  let primes = []; uniq_primes = [], counts = [], counts_elems = [],
+      elems_min = [];
+
+  while (i < base) {
+    if (_base % i == 0) {
+      primes.push(i);
+      _base = _base / i;
+    } else {
+      i++;
+    }   
+  }
+
+  if (primes.length == 0) {
+    primes.push(base);
+  }
+
+  primes.forEach( (el) => {
+      if (uniq_primes.indexOf(el) == -1) {
+        uniq_primes.push(el);
+      }
+  });
+
+  uniq_primes.forEach( (el) => {
+      while (Math.pow(el, n) < number) {
+          count += Math.floor(number / Math.pow(el, n));
+          n++;
+        }
+      counts.push(count);
+      n = 1;
+      count = 0;
+  });
+
+  uniq_primes.forEach( (i) => {
+    primes.forEach ( (el) => {
+          if (i == el) {
+              count++
+          }
+      });
+      counts_elems.push(count);
+      count = 0;
+  })
+
+  for (let i = 0; i < counts.length; i++) {
+      elems_min.push(counts[i] / counts_elems[i]);
+  }
+
+  let index = elems_min.indexOf(Math.min.apply(null, elems_min));
+  let prime = uniq_primes[index];
+  let elem = counts[index];
+  primes.forEach( (el) => {
+    if (prime == el) {
+        count++;
+    }
+  });
+  return Math.floor(elem / count);
 }
